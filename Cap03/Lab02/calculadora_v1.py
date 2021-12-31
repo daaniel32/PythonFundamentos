2cdfca45b0fb910d2712bc9c79f7e2a60788c5dd
Daniel <daaniel32@gmail.com>
2021-12-31 12:42:54 -0300

    calculadora pronta

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,51 @@
 
 print("\n******************* Python Calculator *******************")
 
+def soma(x, y):
+    return x + y
+
+def sub(x, y):
+    return  x - y
+
+def div(x,y):
+    return x / y
+
+def mult(x,y):
+    return x * y
+
+print('Digite o número da operação desejada: ')
+print('1 - Soma')
+print('2 - Subtração')
+print('3 - Divisão')
+print('4 - Multiplicação')
+
+op = input(' \nDigite a sua opção (1/2/3/4): ')
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if op == '1':
+    print('\n')
+    print(num1, "+ ", num2, "=" , soma(num1, num2))
+    print('\n')
+
+elif op == '2':
+    print('\n')
+    print(num2, "-", num2, "=", sub(num1, num2))
+    print('\n')
+
+elif op == '3':
+    print('\n')
+    print(num2, "/", num2, "=", div(num1, num2))
+    print('\n')
+
+elif op == '4':
+    print('\n')
+    print(num2, "*", num2, "=", sub(num1, num2))
+    print('\n')
+
+else:
+    print("Opção Inválida")
+    print('\n')
+    print("Digite uma Opção Válida")
 
